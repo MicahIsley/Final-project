3abf25528c771b648439eede27a3a6c40a1186a1
Micah Isley <micahisley@Micahs-MacBook-Air.local>
2017-09-11 10:43:59 -0400

    moved files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = function(app) {
 	app.get("/", function(req, res) {
-		res.sendFile(path.join(__dirname + "/../animalHome.html"));
+		res.sendFile(path.join(__dirname + "/../public/views/landing.html"));
 	});
 
 	app.get("/forage", function(req, res) {
